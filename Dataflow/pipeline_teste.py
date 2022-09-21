d8d92e188c277c6406b6769fe12a171d84ca931e
Ruckert <85366084+Ruck4s@users.noreply.github.com>
2022-09-20 23:18:26 -0300

    Update pipeline_teste.py

--- Dataflow/pipeline_teste.py
+++ Dataflow/pipeline_teste.py
@@ -1,4 +1,4 @@
-# O arquivo utilizado para esse teste está no caminho Dataflow/titanic.csv.
+# O arquivo utilizado para esse teste é grande para ser posto no repositório, tentarei achar um arquivo menor para o teste.
 # Esse teste foi feito em ambiente local, por isso, modificar o caminho de leitura de arquivo de acordo com cada projeto.
 # Necessário baixar as bibliotecas Apache_beam e Pandas, com o comando: pip install (NOME DA BIBLIOTECA).
 
@@ -7,13 +7,11 @@ import pandas as pd
 
 pipeline = beam.Pipeline()
 
-#Apaga a coluna "Survived" do arquivo.
 def apagar_coluna(df):
-    df = pd.read_csv("/home/ruckert/testes/titanic.csv")
-    data = df.drop(columns=['Survived'])
+    df = pd.read_csv("/home/ruckert/testes/age_gender.csv")
+    data = df.drop(columns=['img_name', 'pixels'])
     return data
 
-#Transforma o DataFrame "data" em um dicionario com o arquivo modificado.
 def df_para_dicionario(data):
     d = dict([(i, [a, b]) for i, a, b in zip(
         data['age'], data['ethnicity'], data['gender'])])
@@ -21,7 +19,7 @@ def df_para_dicionario(data):
 
 transformacao = (
     pipeline
-    |"Leitura do arquivo" >> beam.io.ReadFromText("/home/ruckert/testes/titanic.csv", skip_header_lines=1)
+    |"Leitura do arquivo" >> beam.io.ReadFromText("/home/ruckert/testes/age_gender.csv", skip_header_lines=1)
     |"Apagar colunas" >> beam.Map(apagar_coluna)
     |"Transformar em dicionario" >> beam.Map(df_para_dicionario)
     |"Escrever resultado" >> beam.io.WriteToText("/home/ruckert/testes/resultados.txt")
